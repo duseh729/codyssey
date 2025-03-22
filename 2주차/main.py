2afd14634241b17552f00f9689bf403f24bd875f
안도연 <duseh729@naver.com>
2025-03-22 22:49:16 +0900

    feat: 예외처리 및 이진 파일 저장 및 불러오기 기능 구현

--- 2주차/main.py
+++ 2주차/main.py
@@ -1,15 +1,39 @@
 import os
 import csv
+import pickle
 
 uri = os.path.dirname(__file__) + '/'
 
+# 예외 처리 함수
+def safe_execute(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"에러: 파일을 찾을 수 없습니다. ({args[0]})")
+    except PermissionError:
+        raise PermissionError(f"에러: 파일에 대한 권한이 없습니다. ({args[0]})")
+    except Exception as e:
+        raise Exception(f"알 수 없는 에러 발생: {e}")
+
 # csv 파일 읽어오고 list로 리턴하는 함수
 def read_csv(file_name):
+    return safe_execute(_read_csv, file_name)
+
+def _read_csv(file_name):
     with open(uri + file_name, 'r') as c:
         csv_data = csv.reader(c)
-        
         return list(csv_data)
 
+# csv 파일 저장 함수
+def save_to_csv_file(data, name):
+    return safe_execute(_save_to_csv_file, data, name)
+
+def _save_to_csv_file(data, name):
+    with open(uri + name + '.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+        print("csv 파일 저장 완료")
+        
 # 특정 행으로 정렬해주는 함수
 def sort_matrix(matrix, index, reverse=False):
     return [matrix[0], 
@@ -21,6 +45,23 @@ def filter_high_flammability(matrix, identifier, threshold):
     
     return [matrix[0], 
                 *filter(lambda x: float(x[identifier_index])>=threshold, matrix[1:])]
+
+# 이진 파일 저장 함수
+def save_to_binary_file(data, name):
+    return safe_execute(_save_to_binary_file, data, name)
+
+def _save_to_binary_file(data, name):
+    with open(uri + name + '.bin', 'wb') as f:
+        pickle.dump(data, f)
+        print("이진 파일 저장 완료")
+
+# 이진 파일 읽어서 데이터 리턴하는 함수
+def load_binary_file(name):
+    return safe_execute(_load_binary_file, name)
+
+def _load_binary_file(name):
+    with open(uri + name + '.bin', 'rb') as f:
+        return pickle.load(f)
 
 if __name__ == '__main__':
     csv_list = read_csv('Mars_Base_Inventory_List.csv')
@@ -37,8 +78,10 @@ if __name__ == '__main__':
     for f in filterd_csv_list:
         print(f)
 
-    with open(uri + 'Mars_Base_Inventory_danger.csv', 'w', newline='') as f:
-        write = csv.writer(f)
+    save_to_csv_file(filterd_csv_list, 'Mars_Base_Inventory_danger')
 
-        write.writerows(filterd_csv_list)
+    save_to_binary_file(sorted_csv_list, 'Mars_Base_Inventory_List')
 
+    loaded_binary_data = load_binary_file('Mars_Base_Inventory_List')
+    print('#############loaded binary data################')
+    print(loaded_binary_data)
